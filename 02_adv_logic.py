44470263df3409cd0560287cad98f8501c71627b
Soham-Debnath <20debnathsoham04@gmail.com>
2024-12-31 00:14:20 +0530

    adv_logic

---
+++ 02_adv_logic.py
@@ -0,0 +1,20 @@
+'''
+else:
+    if (computer == 1 and you ==-1):       computer - you = 2
+        print("You Lose!")
+    elif (computer == 1 and you ==0):      computer - you = 1
+        print("You Win!")
+    elif (computer == 0 and you ==-1):     computer - you = 1
+        print("You Win!")
+    elif (computer == 0 and you ==1):      computer - you = -1
+        print("You Lose!")
+    elif (computer == -1 and you ==1):     computer - you = -2
+        print("You Win!")
+    elif (computer == -1 and you ==0):     computer - you = -1
+        print("You Lose!")
+    else:
+        print("something went wrong!")
+
+    You win only when (computer - you) = -2 or 1
+'''
+
